8fb87cb2c484117d5a8e72f07322339c61237286
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-12 11:25:47 -0400

    initial commit

--- rangers.py
+++ rangers.py
@@ -1,3 +1,5 @@
+
+# class constructor
 class Ranger:
   species = "human"
   def __init__(self, name, color, zord):
@@ -5,13 +7,15 @@ class Ranger:
     self.color = color
     self.zord = zord
 
+# an instance method
   def morph(self):
     print(f"{self.zord}! It's morphin time!")
 
-  # @staticmethod
+  # a static method
   def juicebar():
     print("Alpha 5 says meet at the juicebar, Rangers!")
 
+# instantiate some rangers
 Jason = Ranger("Jason", "Red", "Tyrannasorus")
 Tommy = Ranger("Tommy", "Green", "Dragonzord")
 Zack = Ranger("Zack", "Black", "Mastodon")
@@ -19,8 +23,10 @@ Kimberly = Ranger("Kimberly", "Pink", "Pterodactyl")
 Trini = Ranger("Trini", "Yellow", "Sabre Tooth Tiger")
 Billy = Ranger("Billy", "Blue", "Triceratops")
 
+# call the static method
 Ranger.juicebar()
 
+# call the isntance method.  note the two acceptable forms of syntax to do so
 Jason.morph()
 Ranger.morph(Trini)
 Billy.morph()
